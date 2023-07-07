5091da72676827fa8150e5051a4cfa504610564e
张学强 <zhang.xueqiang>
2023-07-07 17:52:12 +0800

    fix: render找不到router-view-item

--- src/components/form-render/container-item/components-mixin.js
+++ src/components/form-render/container-item/components-mixin.js
@@ -2,11 +2,13 @@ import dynamicFormItem from './dynamic-form-item.vue';
 import dynamicTableItem from './dynamic-table-item.vue';
 import FieldComponents from '@/components/form-designer/form-widget/field-widget/index';
 import localCodeItem from './local-code-item.vue';
+import routerViewItem from './router-view-item.vue';
 export default {
   components: {
     'dynamic-form-item': dynamicFormItem,
     'dynamic-table-item': dynamicTableItem,
     'local-code-item': localCodeItem,
+    'router-view-item': routerViewItem,
     ...FieldComponents,
   },
 };
